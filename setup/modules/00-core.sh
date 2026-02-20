de0440d878f99fc862898f55b73608a1546e4701
McEveritts <armyworkbs@gmail.com>
2026-02-19 23:46:36 -0500

    fix(setup): automate Next.js and Go deployment, patch security vulnerabilities

---
+++ setup/modules/00-core.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+# Module: 00-core.sh
+
+function _string() { perl -le 'print map {(a..z,A..Z,0..9)[rand 62] } 0..pop' 15; }
+#################################################################################
+# shellcheck disable=2005,2034,2120,2215,2164,2312
+
+function _bashrc() {
+	\cp -f ${local_setup}templates/bashrc.template /root/.bashrc
+	profile="/root/.profile"
+	if [[ ! -f "${profile}" ]]; then
+		\cp -f ${local_setup}templates/profile.template /root/.profile
+	fi
+}
+
+# intro function (1)
+# shellcheck disable=2005,2312
+function _intro() {
+	DISTRO=$(lsb_release -is)
+	CODENAME=$(lsb_release -cs)
+	SETNAME=$(lsb_release -rc)
+	echo
+	echo
+	echo "[${repo_title}AetherFlow${normal}] ${title} AetherFlow Seedbox Installation ${normal}  "
+	echo
+	echo "   ${title}              Heads Up!              ${normal} "
+	echo "   ${message_title}  AetherFlow works with the following  ${normal} "
+	echo "   ${message_title}  Ubuntu 20.04+ / Debian 11+ ${normal} "
+	echo
+	echo
+	echo "${green}Checking distribution ...${normal}"
+	if [[ ! -x /usr/bin/lsb_release ]]; then
+		echo "It looks like you are running ${DISTRO}, which is not supported by AetherFlow."
+		echo "Exiting..."
+		exit 1
+	fi
+	echo "$(lsb_release -a)"
+	echo
+	if [[ ! "${DISTRO}" =~ ("Ubuntu"|"Debian") ]]; then
+		echo "${DISTRO}: ${alert} It looks like you are running ${DISTRO}, which is not supported by AetherFlow ${normal} "
+		echo 'Exiting...'
+		exit 1
+	elif [[ ! "${CODENAME}" =~ ("bionic"|"focal"|"jammy"|"noble"|"bullseye"|"bookworm"|"trixie") ]]; then
+		echo "Oh drats! You do not appear to be running a supported ${DISTRO} release."
+		echo "${bold}${SETNAME}${normal}"
+		echo 'Exiting...'
+		exit 1
+	fi
+
+	# Set PHP Version based on OS
+	if [[ "${CODENAME}" =~ ("bionic"|"buster") ]]; then
+		PHP_VER="7.4"
+	elif [[ "${CODENAME}" =~ ("bullseye") ]]; then
+		PHP_VER="7.4"
+	elif [[ "${CODENAME}" =~ ("bookworm") ]]; then
+		PHP_VER="8.2"
+	elif [[ "${CODENAME}" =~ ("trixie") ]]; then
+		PHP_VER="8.3"
+	elif [[ "${CODENAME}" =~ ("focal") ]]; then
+		PHP_VER="7.4"
+	elif [[ "${CODENAME}" =~ ("jammy") ]]; then
+		PHP_VER="8.1"
+	elif [[ "${CODENAME}" =~ ("noble") ]]; then
+		PHP_VER="8.3"
+	else
+		PHP_VER="8.2"
+	fi
+	export PHP_VER
+	echo "Selected PHP Version: ${PHP_VER}"
+}
+
+# check if root function (2)
+function _checkroot() {
+	if [[ ${EUID} != 0 ]]; then
+		echo 'This script must be run with root privileges.'
+		echo 'Exiting...'
+		exit 1
+	fi
+	echo "${green}Congrats! You're running as root. Let's continue${normal} ... "
+	echo
+}
+
+# check if distribution kernel function (3)
+# shellcheck disable=2312,2162
+function _checkkernel() {
+	grsec=$(uname -a | grep -i grs)
+	if [[ -n ${grsec} ]]; then
+		echo -e "Your server is currently running with kernel version: $(uname -r)"
+		echo -e "Kernels with ${bold}grsec${normal} are not supported"
+		echo -ne "${bold}${yellow}Would you like AetherFlow to install the distribution kernel?${normal} (Default: ${green}${bold}Y${normal}) "
+		read input
+		case ${input} in
+		[yY] | [yY][Ee][Ss] | "")
+			kernel=yes
+			echo "Your distribution's default kernel will be installed. A reboot will be ${bold}required${normal}."
+			;;
+		[nN] | [nN][Oo])
+			echo "Installer will not continue. Exiting ... "
+			exit 0
+			;;
+		*)
+			kernel=yes
+			echo "Your distribution's default kernel will be installed. A reboot will be ${bold}required${normal}."
+			;;
+		esac
+		if [[ ${kernel} == yes ]]; then
+			if [[ ${DISTRO} == Ubuntu ]]; then
+				apt-get install -q -y linux-image-generic >>"${OUTTO}" 2>&1
+			elif [[ ${DISTRO} == Debian ]]; then
+				arch=$(uname -m)
+				if [[ ${arch} =~ ("i686"|"i386") ]]; then
+					apt-get install -q -y linux-image-686 >>"${OUTTO}" 2>&1
+				elif [[ ${arch} == x86_64 ]]; then
+					apt-get install -q -y linux-image-amd64 >>"${OUTTO}" 2>&1
+				fi
+			fi
+			mv /etc/grub.d/06_OVHkernel /etc/grub.d/25_OVHkernel
+			update-grub >>"${OUTTO}" 2>&1
+		fi
+	fi
+}
+
+# check if create log function (4)
+# shellcheck disable=2162
+function _logcheck() {
+	echo -ne "${bold}${yellow}Do you wish to write to a log file?${normal} (Default: ${green}${bold}Y${normal}) "
+	read input
+	case ${input} in
+	[yY] | [yY][Ee][Ss] | "")
+		OUTTO="/root/AetherFlow.${PPID}.log"
+		echo "${bold}Output is being sent to /root/AetherFlow.${magenta}${PPID}${normal}${bold}.log${normal}"
+		;;
+	[nN] | [nN][Oo])
+		OUTTO="/dev/null 2>&1"
+		echo "${cyan}NO output will be logged${normal}"
+		;;
+	*)
+		OUTTO="/root/AetherFlow.${PPID}.log"
+		echo "${bold}Output is being sent to /root/AetherFlow.${magenta}${PPID}${normal}${bold}.log${normal}"
+		;;
+	esac
+	if [[ ! -d /root/tmp ]]; then
+		sed -i 's/noexec,//g' /etc/fstab
+		mount -o remount /tmp >>"${OUTTO}" 2>&1
+	fi
+}
+
+# setting system hostname function (5)
+# shellcheck disable=2162
+function _hostname() {
+	echo -ne "Please enter a hostname for this server (${bold}Hit ${standout}${green}ENTER${normal} to make no changes${normal}): "
+	read input
+	if [[ -z ${input} ]]; then
+		echo "No hostname supplied, no changes made!!"
+	else
+		hostname ${input}
+		echo "${input}" >/etc/hostname
+		echo "127.0.0.1 ${input}" >/etc/hosts
+		echo "Hostname set to ${input}"
+	fi
+	echo
+}
+
+# setting locale function
+function _locale() {
+	echo 'LANGUAGE="en_US.UTF-8"' >>/etc/default/locale
+	echo 'LC_ALL="en_US.UTF-8"' >>/etc/default/locale
+	echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
+	if [[ -e /usr/sbin/locale-gen ]]; then
+		locale-gen >>"${OUTTO}" 2>&1
+	else
+		apt-get -y update >>"${OUTTO}" 2>&1
+		apt-get install locales -y >>"${OUTTO}" 2>&1
+		locale-gen >>"${OUTTO}" 2>&1
+		export LANG="en_US.UTF-8"
+		export LC_ALL="en_US.UTF-8"
+		export LANGUAGE="en_US.UTF-8"
+	fi
+}
---
+++ setup/modules/05-finalize.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+# Module: 05-finalize.sh
+
+
+# function to configure pure-ftpd
+function _ftpdconfig() {
+	\cp -f ${local_setup}templates/openssl.cnf.template /root/.openssl.cnf
+
+	#openssl req -config /root/.openssl.cnf -x509 -nodes -days 365 -newkey rsa:1024 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem >/dev/null 2>&1
+	openssl req -config /root/.openssl.cnf -x509 -nodes -days 720 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem >/dev/null 2>&1
+
+	\cp -f ${local_setup}templates/vsftpd.conf.template /etc/vsftpd.conf
+
+	# Enabling passive mode
+	sed -i 's/^\(pasv_min_port=\).*/\110090/' /etc/vsftpd.conf
+	sed -i 's/^\(pasv_max_port=\).*/\110100/' /etc/vsftpd.conf
+	echo "pasv_address="${IP} >>/etc/vsftpd.conf
+	iptables -I INPUT -p tcp --destination-port 10090:10100 -j ACCEPT
+
+	echo "" >/etc/vsftpd.chroot_list
+}
+
+# The following function makes necessary changes to Network and TZ settings needed for
+# the proper functionality of the AetherFlow Dashboard.
+function _quickstats() {
+	# Dynamically adjust to use the servers active network adapter
+	IFACE=$(ip route get 8.8.8.8 | awk 'NR==1 {print $5}')
+	printf "${IFACE}" >/srv/rutorrent/home/db/interface.txt
+	sed -i "s/INETFACE/${IFACE}/g" /srv/rutorrent/home/inc/config.php
+	printf "${username}" >/srv/rutorrent/home/db/master.txt
+	# Use server timezone
+	cd /usr/share/zoneinfo
+	find ./* -type f -exec sh -c "diff -q /etc/localtime '{}' > /dev/null && echo {}" \; >~/tz.txt
+	cd ~
+	LOCALE=en_GB.UTF-8
+	LANG=lang_en
+	sed -i "s/LOCALE/${LOCALE}/g" /srv/rutorrent/home/inc/localize.php
+	sed -i "s/LANG/${LANG}/g" /srv/rutorrent/home/inc/localize.php
+	#echo "  date_default_timezone_set('$(cat tz.txt)');" >> /srv/rutorrent/home/widgets/config.php
+	#echo "" >> /srv/rutorrent/home/widgets/config.php
+	#echo "?>" >> /srv/rutorrent/home/widgets/config.php
+	if [[ ${primaryroot} == "home" ]]; then
+		cd /srv/rutorrent/home/widgets && rm disk_data.php
+		mv disk_datah.php disk_data.php
+		chown -R www-data:www-data /srv/rutorrent/home/widgets
+	else
+		rm /srv/rutorrent/home/widgets/disk_datah.php
+	fi
+}
+
+# initializing console info
+# shellcheck disable=2312
+function _quickconsole() {
+	PUBLICIP=$(ip route get 8.8.8.8 | awk 'NR==1 {print $7}')
+	cat >/etc/profile<<EOF
+echo " Welcome Back !"
+echo "    * Dashboard:  https://${PUBLICIP}"
+echo "    * Support:    https://plaza.AetherFlow.io"
+echo "    * Donate:     https://AetherFlow.io/donate"
+echo ""
+EOF
+	apt-get -y install shellinabox >>"${OUTTO}" 2>&1
+
+	\cp -f ${local_setup}templates/sysd/shellinabox.template /etc/systemd/system/shellinabox.service
+	\cp -f ${local_setup}templates/quickconsole/00_QuickConsole.css.template /etc/shellinabox/options-enabled/00_QuickConsole.css
+	\cp -f ${local_setup}templates/web-console.conf.template /etc/apache2/sites-enabled/${username}.console.conf
+
+	sed -i "s/PUBLICIP/${PUBLICIP}/g" /etc/apache2/sites-enabled/${username}.console.conf
+	sed -i "s/USER/${username}/g" /etc/apache2/sites-enabled/${username}.console.conf
+
+	chmod +x /etc/shellinabox/options-enabled/00_QuickConsole.css
+	chmod 777 /etc/shellinabox/options-enabled/00_QuickConsole.css
+
+	systemctl enable shellinabox.service >/dev/null 2>&1
+	systemctl stop shellinabox.service >/dev/null 2>&1
+	systemctl daemon-reload >/dev/null 2>&1
+	systemctl start shellinabox.service >/dev/null 2>&1
+
+}
+
+# seedbox boot for first user
+function _boot() {
+	#if [[ $cron == yes ]]; then
+	#  touch /install/.cron.lock
+	#  command1="*/1 * * * * /home/${username}/.startup >/dev/null 2>&1"
+	#  cat <(fgrep -iv "${command1}" <(sh -c 'sudo -u ${username} crontab -l' >/dev/null 2>&1)) <(echo "${command1}") | sudo -u ${username} crontab -
+	#elif [[ $cron == no ]]; then
+	\cp -f ${local_setup}templates/sysd/rtorrent.template /etc/systemd/system/rtorrent@.service >/dev/null 2>&1
+	\cp -f ${local_setup}templates/sysd/autodlirssi.template /etc/systemd/system/irssi@.service >/dev/null 2>&1
+	\cp -f ${local_setup}templates/sysd/deluged.template /etc/systemd/system/deluged@.service >/dev/null 2>&1
+	\cp -f ${local_setup}templates/sysd/deluge-web.template /etc/systemd/system/deluge-web@.service >/dev/null 2>&1
+	systemctl enable {rtorrent,irssi,deluged,deluge-web}@${username} >/dev/null 2>&1
+	systemctl start {rtorrent,irssi,deluged,deluge-web}@${username} >/dev/null 2>&1
+	#fi
+
+	echo "*/1 * * * * root bash /usr/local/bin/AetherFlow/system/set_interface" >/etc/cron.d/set_interface
+	/usr/local/bin/AetherFlow/system/set_interface >/dev/null 2>&1
+}
+
+# function to set permissions on first user
+function _perms() {
+	chown -R ${username}.${username} /home/${username}/ >>"${OUTTO}" 2>&1
+	sudo -u ${username} chmod 755 /home/${username}/ >>"${OUTTO}" 2>&1
+	chmod +x /etc/cron.daily/denypublic >/dev/null 2>&1
+	chmod 777 /home/${username}/.sessions >/dev/null 2>&1
+	chown -R www-data:www-data /srv/rutorrent/home >/dev/null 2>&1
+	if [[ ${tr} == "yes" ]]; then
+		chown -R ${username}:debian-transmission /home/${username}/torrents/transmission >/dev/null 2>&1
+		service transmission-daemon reload >>"${OUTTO}" 2>&1
+	fi
+}
+
+# BBR function
+function _insbbr() {
+	cd /tmp
+	\cp -rf ${local_setup}sources/BBR/BBR.sh .
+	chmod a+x BBR.sh
+	bash BBR.sh -f v4.14.32 >>"${OUTTO}" 2>&1
+	cd
+}
+
+# fix bcm NIC firmware
+function _insbcm() {
+	mkdir -p ${local_setup}/bcm
+	cd ${local_setup}/bcm
+	git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git >>"${OUTTO}" 2>&1
+	mkdir -p /lib/firmware/bnx2/
+	\cp -rf ${local_setup}/bcm/linux-firmware/bnx2/ /lib/firmware
+}
+
+# function to create ssl cert for pure-ftpd ()
+function _pureftpcert() {
+	/bin/true
+}
+################################################################################
+#   //END UNUSED FUNCTIONS//
+################################################################################
+
+# function to show finished data
+# shellcheck disable=2312,2162,2249,2250
+function _finished() {
+	ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $7}')
+	echo
+	echo
+	echo -e " ${black}${on_green}    [AetherFlow] Seedbox & GUI Installation Completed    ${normal} "
+	echo -e "        ${standout}    INSTALLATION COMPLETED in ${FIN}/min    ${normal}             "
+	echo
+	echo
+	echo "  Valid Commands:  "
+	echo '  -------------------'
+	echo
+	echo -e " ${green}createSeedboxUser${normal} - creates a shelled seedbox user"
+	echo -e " ${green}deleteSeedboxUser${normal} - deletes a created seedbox user and their directories"
+	echo -e " ${green}changeUserpass${normal} - change users SSH/FTP/ruTorrent password"
+	echo -e " ${green}setdisk${normal} - set your disk quota for any given user"
+	echo -e " ${green}showspace${normal} - shows the amount of space used by all users on the server"
+	echo -e " ${green}reload${normal} - restarts your seedbox services, i.e; rtorrent & irssi"
+	echo -e " ${green}upgradeBTSync${normal} - upgrades BTSync when new version is available"
+	echo -e " ${green}upgradeOmbi${normal} - upgrades Ombi when new version is available"
+	echo -e " ${green}upgradePlex${normal} - upgrades Plex when new version is available"
+	echo -e " ${green}box install letsencrypt${normal} - installs a valid SSL certificate to be used with "
+	echo -e "                           a valid domain name. "
+	echo -e " ${green}box${normal} - type 'box -h' for a summary of how to use box!"
+	echo
+	echo
+	echo
+	echo '################################################################################################'
+	echo "#   Seedbox can be found at https://${username}:${passwd}@${ip} "
+	echo "#   ${cyan}(Also works for FTP:5757/SSH:4747)${normal}"
+	echo "#   If you need to restart rtorrent/irssi, you can type 'reload'"
+	echo "#   https://${username}:${passwd}@${ip} (Also works for FTP:5757/SSH:4747)" >${username}.info
+	echo "#   Reloading: ${green}sshd${normal}, ${green}apache${normal}, ${green}memcached${normal}, ${green}php${PHP_VER}${normal}, ${green}vsftpd${normal} and ${green}fail2ban${normal}"
+	echo '################################################################################################'
+	echo
+	cat >/root/information.info <<EOF
+  Seedbox can be found at https://${username}:${passwd}@${ip} (Also works for FTP:5757/SSH:4747)
+  If you need to restart rtorrent/irssi, you can type 'reload'
+  https://${username}:${passwd}@${ip} (Also works for FTP:5757/SSH:4747)
+EOF
+	rm -rf "$0" >>"${OUTTO}" 2>&1
+	for i in ssh apache2 php"${PHP_VER}"-fpm vsftpd fail2ban quota memcached cron; do
+		service "${i}" restart >>"${OUTTO}" 2>&1
+		systemctl enable "${i}" >>"${OUTTO}" 2>&1
+	done
+	rm -rf /root/tmp/
+	echo -ne "  Do you wish to reboot (recommended!): (Default ${green}Y${normal})"
+	echo -ne "  Do you wish to reboot (recommended!): (Default ${green}Y${normal})"
+	read reboot_response
+	case $reboot_response in
+	[yY] | [yY][Ee][Ss] | "") systemctl reboot ;;
+	[nN] | [nN][Oo]) echo "  ${cyan}Skipping reboot${normal} ... " ;;
+	esac
+}
