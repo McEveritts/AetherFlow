a0ad3a54ff62eb9fe403d2f6c371d43c3c8fddf5
McEveritts <armyworkbs@gmail.com>
2026-02-23 11:25:44 -0500

    feat: Add service status verification to packages

--- backend/models/package.go
+++ backend/models/package.go
@@ -7,6 +7,8 @@ type Package struct {
 	LockFile    string `json:"lockFile"`
 	Category    string `json:"category"`
 	// Additional fields for the response
-	Hits   int    `json:"hits"`
-	Status string `json:"status"`
+	Hits        int    `json:"hits"`
+	Status      string `json:"status"`
+	ServiceType string `json:"serviceType"`
+	ServiceName string `json:"serviceName"`
 }
